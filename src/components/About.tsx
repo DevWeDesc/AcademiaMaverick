c9854e1aa5036357dc8b0b786409c01da213a357
ViniHGV <ixxvinicius@gmail.com>
2023-10-18 14:05:36 -0300

    Adicionando Swiper do About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { SwiperAbout } from "./SwiperAbout";
+import { SwiperAbout } from "./SwiperAbout/SwiperAbout";
 
 export const About = () => {
   return (
-    <div className="flex flex-col pt-20 px-5">
-      <h2 className="font-bold text-2xl">Nossa História!</h2>
-      <div className="w-[3rem] h-[2px] bg-logoColor mb-5" />
-      <p className="text-logoColorLight opacity-100 mb-2">
-        Construindo um Legado de Excelência em Fitness
-      </p>
-      <p className="opacity-60 text-[12px] mb-7">
-        Descubra a nossa jornada de dedicação ao fitness, paixão pela saúde e
-        compromisso com a sua transformação física. Conheça a equipe que faz a
-        diferença em nossa academia e saiba por que somos a escolha certa para
-        atingir seus objetivos de bem-estar
-      </p>
+    <>
+      <div className="flex flex-col pt-20 px-5">
+        <h2 className="font-bold text-2xl">Nossa História!</h2>
+        <div className="w-[3rem] h-[2px] bg-logoColor mb-5" />
+        <p className="text-logoColorLight opacity-100 mb-2">
+          Construindo um Legado de Excelência em Fitness
+        </p>
+        <p className="opacity-60 text-[12px] mb-7">
+          Descubra a nossa jornada de dedicação ao fitness, paixão pela saúde e
+          compromisso com a sua transformação física. Conheça a equipe que faz a
+          diferença em nossa academia e saiba por que somos a escolha certa para
+          atingir seus objetivos de bem-estar
+        </p>
+      </div>
       <SwiperAbout />
-    </div>
+    </>
   );
 };
---
+++ src/components/SwiperAbout/SwiperAbout.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "./styles.css";
+import ImageSwiper from "../../../public/Image.png";
+import Image from "next/image";
+
+export const SwiperAbout = () => {
+  return (
+    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      <SwiperSlide>
+        <Image alt="" src={ImageSwiper}></Image>
+      </SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
+  );
+};
