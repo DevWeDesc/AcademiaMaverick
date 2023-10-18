5c65cf67a19911c4759eacfe97413e99ea71231e
ViniHGV <ixxvinicius@gmail.com>
2023-10-18 13:51:48 -0300

    Importando os components para a HomePage4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { About } from "@/components/About";
+import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <main className=""></main>
+      <main className="">
+        <section id="init">
+          <Banner />
+        </section>
+        <section>
+          <About />
+        </section>
+      </main>
     </div>
   );
 }
