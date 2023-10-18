2140d1cc7cc10bd8432831f683e058b4a1520a41
ViniHGV <ixxvinicius@gmail.com>
2023-10-18 14:17:33 -0300

    Criando Swiper About

--- src/components/SwiperAbout/SwiperAbout.tsx
+++ src/components/SwiperAbout/SwiperAbout.tsx
@@ -15,14 +15,12 @@ export const SwiperAbout = () => {
       <SwiperSlide>
         <Image alt="" src={ImageSwiper}></Image>
       </SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
+      <SwiperSlide>
+        <Image alt="" src={ImageSwiper}></Image>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image alt="" src={ImageSwiper}></Image>
+      </SwiperSlide>
     </Swiper>
   );
 };
