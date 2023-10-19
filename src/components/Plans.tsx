ab0a0e2357d9aa8a12551a11f36a77d11c1a6c8d
ViniHGV <ixxvinicius@gmail.com>
2023-10-19 11:38:08 -0300

    Arrumando estilização

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -4,11 +4,11 @@ import { DataPlans } from "../../data/data";
 
 export const Plans = () => {
   return (
-    <div className="flex flex-col px-5 py-8 gap-5 items-center">
+    <div className="flex flex-col px-5 pt-20 gap-5 items-center">
       <p className="text-logoColorLight font-semibold">
         Nossos Planos Acessíveis e Flexíveis
       </p>
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-center mb-3 gap-2">
         <h2 className="text-2xl text-center font-bold">
           Escolha o Plano Perfeito para Você
         </h2>
