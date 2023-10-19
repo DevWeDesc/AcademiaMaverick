ff858bd75ac03dbee5a6711b6c91eab6d4a8a9f9
ViniHGV <ixxvinicius@gmail.com>
2023-10-19 09:50:56 -0300

    Finalizando Sessão de Feedbacks para mobike

--- src/components/SwiperFeedbacks/Swiper.tsx
+++ src/components/SwiperFeedbacks/Swiper.tsx
@@ -5,21 +5,78 @@ import { Navigation } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
-import "./styles.css";
+import "./style.css";
 import ImageSwiper from "../../../public/Image.png";
 import Image from "next/image";
+import ImagePeople from "../../../public/Ellipse 2.png";
+import stars from "../../../public/Frame 13.png";
 
 export const SwiperFeedbacks = () => {
   return (
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+    <Swiper
+      navigation={true}
+      modules={[Navigation]}
+      className="mySwiperFeedback"
+    >
       <SwiperSlide>
-        <Image alt="" src={ImageSwiper}></Image>
+        <div className="w-full flex cursor-move mt-7 flex-col gap-4 p-2 rounded-xl bg-[#181818]">
+          <div className="flex gap-6 m-0 items-center">
+            <Image
+              className="max-w-[56px] max-h-[56px]"
+              alt=""
+              src={ImagePeople}
+            />
+            <div className="flex flex-col gap-1">
+              <p className="font-bold">Vinicius Henrique</p>
+              <Image alt="" src={stars} className="w-[86px]"></Image>
+            </div>
+          </div>
+          <p className="text-left text-[12px] font-medium">
+            Esta academia é simplesmente incrível! Desde o momento em que
+            entrei, fui recebido com uma equipe calorosa e amigável, além de
+            instrutores experientes e motivadores.
+          </p>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        <Image alt="" src={ImageSwiper}></Image>
+        <div className="w-full flex mt-7 flex-col gap-4 p-2 rounded-xl bg-[#181818]">
+          <div className="flex gap-6 m-0 items-center">
+            <Image
+              className="max-w-[56px] max-h-[56px]"
+              alt=""
+              src={ImagePeople}
+            />
+            <div className="flex flex-col gap-1">
+              <p className="font-bold">Vinicius Henrique</p>
+              <Image alt="" src={stars} className="w-[86px]"></Image>
+            </div>
+          </div>
+          <p className="text-left text-[12px] font-medium">
+            Esta academia é simplesmente incrível! Desde o momento em que
+            entrei, fui recebido com uma equipe calorosa e amigável, além de
+            instrutores experientes e motivadores.
+          </p>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        <Image alt="" src={ImageSwiper}></Image>
+        <div className="w-full flex mt-7 flex-col gap-4 p-2 rounded-xl bg-[#181818]">
+          <div className="flex gap-6 m-0 items-center">
+            <Image
+              className="max-w-[56px] max-h-[56px]"
+              alt=""
+              src={ImagePeople}
+            />
+            <div className="flex flex-col gap-1">
+              <p className="font-bold">Vinicius Henrique</p>
+              <Image alt="" src={stars} className="w-[86px]"></Image>
+            </div>
+          </div>
+          <p className="text-left text-[12px] font-medium">
+            Esta academia é simplesmente incrível! Desde o momento em que
+            entrei, fui recebido com uma equipe calorosa e amigável, além de
+            instrutores experientes e motivadores.
+          </p>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
