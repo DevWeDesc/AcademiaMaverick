fdce8b2f462a17c2d0dfe4d96e5ac475b4273aef
ViniHGV <ixxvinicius@gmail.com>
2023-10-20 10:05:42 -0300

    Responsividade do banner para DEsktop

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import React from "react";
 import ImageBannerMobile from "../../public/ImageBannerMobile.png";
+import ImageBannerDektop from "../../public/ImageBannerDektop.png";
 
 export const Banner = () => {
   return (
     <div>
-      <div className="pt-[80px] w-screen h-auto lg:hidden">
-        <Image className="w-full" alt="" src={ImageBannerMobile} />
-        <div className="relative bottom-10 flex flex-col items-center gap-1 p-5">
-          <h2 className="text-2xl font-bold text-center max-w-[18rem]">
+      <div className="pt-[80px] w-screen h-auto lg:grid lg:grid-cols-2">
+        <Image className="w-full lg:hidden" alt="" src={ImageBannerMobile} />
+        <Image className="w-full" alt="" src={ImageBannerDektop} />
+        <div className="relative bottom-10 flex flex-col items-center gap-1 p-5 lg:bottom-0 lg:justify-center lg:items-start lg:w-full lg:px-28">
+          <h2 className="text-2xl font-bold text-center max-w-[18rem] lg:text-5xl lg:text-left lg:min-w-full">
             Transforme seu Corpo e sua Vida!
           </h2>
-          <p className="max-w-[22rem] text-center opacity-60 hover:opacity-90 transition-all">
+          <p className="max-w-[22rem] text-center opacity-60 hover:opacity-90 transition-all lg:text-2xl lg:text-left lg:min-w-full lg:pt-1">
             Alcance Seus Objetivos de Fitness na Nossa Academia Maverick
             Premium!
           </p>
