4e50d3f34d021fa6ba7174fb12eedd7924ced135
ViniHGV <ixxvinicius@gmail.com>
2024-01-09 11:43:45 -0300

    feat: adicionando atividades a DataNav

--- data/data.ts
+++ data/data.ts
@@ -3,15 +3,16 @@ import PhotoPrimaryComent from "../public/ImageWesley.jpeg";
 export const DataNav: IDataNav[] = [
   { name: "Inicio", url: "#init" },
   { name: "Sobre Nós", url: "#about" },
-  { name: "Feedbacks", url: "#feedbacks" },
+  { name: "Atividades", url: "#activies" },
   { name: "Planos", url: "#plans" },
+  { name: "Feedbacks", url: "#feedbacks" },
 ];
 
 export const DataPlans: ICardPlans[] = [
   {
     id: 2,
     title: "Premium Anual",
-    price: "74,90",
+    price: "99,90",
     benefits: [
       { content: "Acesso ilimitado à academia" },
       { content: "Aulas em grupo inclusas" },
@@ -23,7 +24,7 @@ export const DataPlans: ICardPlans[] = [
   {
     id: 3,
     title: "Pro Semestral",
-    price: "92,90",
+    price: "109,90",
     benefits: [
       { content: "Acesso ilimitado à academia" },
       { content: "Aulas em grupo inclusas" },
@@ -34,8 +35,8 @@ export const DataPlans: ICardPlans[] = [
   },
   {
     id: 1,
-    title: "Plus Trimestrol",
-    price: "99,90",
+    title: "Plus Trimestral",
+    price: "119,90",
     benefits: [
       { content: "Acesso ilimitado à academia" },
       { content: "Aulas em grupo inclusas" },
@@ -47,7 +48,7 @@ export const DataPlans: ICardPlans[] = [
   {
     id: 4,
     title: "Basic Mensal",
-    price: "109,90",
+    price: "129,90",
     benefits: [
       { content: "Acesso ilimitado à academia" },
       { content: "Aulas em grupo inclusas" },
@@ -59,7 +60,9 @@ export const DataPlans: ICardPlans[] = [
 ];
 
 export const schedulesData: Ischedules[] = [
-  { days: "Segunda a Sexta", schedules: " das 0:00 as 23:59" },
+  { days: "Segunda", schedules: "Inicio 0:00" },
+  { days: "Terça, Quarta e Quinta", schedules: "24 Horas" },
+  { days: "Sexta", schedules: "Termino 00:00" },
   { days: "Sábado", schedules: "das 8:00 as 18:00" },
   { days: "Domingo", schedules: "das 8:00 as 14:00" },
 ];
