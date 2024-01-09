721bc1b0cd2883a6226f03a0db5a9da651f16d30
ViniHGV <ixxvinicius@gmail.com>
2024-01-09 11:46:52 -0300

    feat: importando imagens para cada atividade, editando estilização e adicionando atividades que faltavam

--- src/components/Activies/GridActivies.tsx
+++ src/components/Activies/GridActivies.tsx
@@ -1,105 +1,242 @@
 import Image from "next/image";
 import React from "react";
 import ImageJumpping from "../../../public/jumpping.jpg";
+import ImageAbs from "../../../public/abdominal.jpg";
+import ImageStep from "../../../public/Step.jpg";
+import ImagePilates from "../../../public/pilates.png";
+import ImageCrossKids from "../../../public/crosskids.jpg";
+import ImageFitDance from "../../../public/fitdance.jpg";
+import ImageCrossTraining from "../../../public/crosstraining.png";
+import ImageGap from "../../../public/gap.jpeg";
+import ImageHiit from "../../../public/hiit.jpg";
 
 export const GridActivies = () => {
   return (
-    <div className="grid grid-cols-4 grid-rows-2 h-[556px] gap-6 text-left">
+    <div className="grid grid-cols-4 grid-rows-2 h-auto gap-6 text-left">
       <div className="relative col-span-2 w-full">
         <Image
-          className="h-full absolute object-cover opacity-20 rounded-lg   bg-[rgb(0,0,0,0.2)]"
+          className="h-full absolute object-cover opacity-20 rounded-lg bg-[rgb(0,0,0,0.2)]"
           src={ImageJumpping}
           alt="Imagem de pulo"
           objectPosition="center"
         />
-        <div className="absolute top-7 left-5 text-white space-y-4">
-          <p className="">Cross Fight</p>
-          <p className="font-extralight text-sm opacity-90 flex flex-col gap-1">
-            <strong>Horário de Atividade</strong>
-            <span>Seg das 07:00hrs ás 08:00hrs</span>
-            <span>Ter e Qui das 20:00hrs ás 21:00hrs</span>
-            <span>Sex das 06:00hrs ás 07:00hrs</span>
-          </p>
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">Cross Fight - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Segunda-feira:</strong>
+              <br />
+              <span>- 07:00hrs às 08:00hrs</span>
+            </p>
+            <p>
+              <strong>Terça-feira e Quinta-feira:</strong> <br />
+              <span> - 20:00hrs às 21:00hrs</span>
+            </p>
+            <p>
+              <strong>Sexta-feira:</strong> <br />
+              <span> - 06:00hrs às 07:00hrs</span>
+            </p>
+          </div>
         </div>
       </div>
       <div className="relative w-full ">
         <Image
-          className="h-full absolute object-cover  opacity-20 rounded-lg bg-[rgb(0,0,0,0.2)]"
-          src={ImageJumpping}
+          className="h-full absolute object-cover  opacity-30 rounded-lg bg-[rgb(0,0,0,0.2)]"
+          src={ImageAbs}
           alt="Imagem de pulo"
           objectPosition="center"
         />
-        <div className="absolute top-7 left-5 text-white space-y-2">
-          <p className="">Pilates</p>
-          <p className="font-extralight opacity-90">
-            Horário de Atividade: <br />
-            Seg: 06:00hrs ás 7:00hrs{" "}
-          </p>
+        <div className=" text-white space-y-4 py-8 px-5">
+          <p className="">Abdominal + Alongamento - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Segunda-feira:</strong>
+              <br />
+              <span>- 20:00hrs às 21:00hrs</span>
+            </p>
+            <p>
+              <strong>Terça-feira:</strong>
+              <br />
+              <span>- 08:00hrs às 09:00hrs</span>
+            </p>
+            <p>
+              <strong>Quarta-feira:</strong>
+              <br />
+              <span>- 06:00hrs às 07:00hrs</span>
+            </p>
+          </div>
         </div>
       </div>{" "}
       <div className="relative w-full">
         <Image
-          className="h-full object-cover opacity-20 rounded-lg bg-[rgb(0,0,0,0.2)]"
-          src={ImageJumpping}
+          className="h-full absolute object-cover opacity-30 rounded-lg bg-[rgb(0,0,0,0.2)]"
+          src={ImageStep}
           alt="Imagem de pulo"
           objectPosition="center"
         />
-        <div className="absolute top-7 left-5 text-white space-y-2">
-          <p className="">Step</p>
-          <p className="font-extralight text-sm opacity-90 flex flex-col gap-1">
-            <strong>Horário de Atividade</strong>
-            <span>Seg das 18:00hrs ás 19:00hrs</span>
-            <span>Qui das 08:00hrs ás 09:00hrs</span>
-          </p>
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">Step - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Segunda-feira:</strong>
+              <br />
+              <span>- 18:00hrs às 19:00hrs</span>
+            </p>
+            <p>
+              <strong>Quinta-feira:</strong>
+              <br />
+              <span>- 08:00hrs às 09:00hrs</span>
+            </p>
+          </div>
         </div>
       </div>
       <div className="relative w-full ">
         <Image
-          className="h-full absolute object-cover  opacity-20 rounded-lg bg-[rgb(0,0,0,0.2)]"
-          src={ImageJumpping}
+          className="h-full absolute object-cover opacity-30 rounded-lg bg-[rgb(0,0,0,0.2)]"
+          src={ImagePilates}
           alt="Imagem de pulo"
           objectPosition="center"
         />
-        <div className="absolute top-7 left-5 text-white space-y-2">
-          <p className="">Abdominal + Alongamento</p>
-          <p className="font-extralight text-sm opacity-90 flex flex-col gap-1">
-            <strong>Horário de Atividade</strong>
-            <span>Seg das 20:00hrs ás 21:00hrs</span>
-            <span>Ter das 08:00hrs ás 09:00hrs</span>
-            <span>Qua das 06:00hrs ás 07:00hrs</span>
-          </p>
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">Pilates - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Segunda-feira:</strong>
+              <br />
+              <span>- 06:00hrs às 07:00hrs</span>
+            </p>
+          </div>
         </div>
       </div>{" "}
       <div className="relative w-full ">
         <Image
-          className="h-full absolute object-cover  opacity-20 rounded-lg bg-[rgb(0,0,0,0.2)]"
-          src={ImageJumpping}
+          className="h-full absolute object-cover  opacity-30 rounded-lg bg-[rgb(0,0,0,0.2)]"
+          src={ImageCrossKids}
           alt="Imagem de pulo"
           objectPosition="center"
         />
-        <div className="absolute top-7 left-5 text-white space-y-2">
-          <p className="">Cross Kids</p>
-          <p className="font-extralight text-sm opacity-90 flex flex-col gap-1">
-            <strong>Horário de Atividade</strong>
-            <span>Ter e Quinta das 18:00hrs ás 19:00hrs</span>
-          </p>
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">Cross Kids - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Terça-feira e Quinta-feira:</strong>
+              <br />
+              <span>- 18:00hrs às 19:00hrs</span>
+            </p>
+          </div>
         </div>
       </div>
       <div className="relative col-span-2 w-full ">
         <Image
           className="h-full absolute object-cover  opacity-20 rounded-lg bg-[rgb(0,0,0,0.2)]"
-          src={ImageJumpping}
+          src={ImageFitDance}
           alt="Imagem de pulo"
           objectPosition="center"
         />
-        <div className="absolute top-7 left-5 text-white space-y-2">
-          <p className="">FitDance</p>
-          <p className="font-extralight text-sm opacity-90 flex flex-col gap-1">
-            <strong>Horário de Atividade</strong>
-            <span>Seg das 08:00hrs ás 09:00hrs e das 19:00hrs ás 20:00hrs</span>
-            <span>Quarta e Sex das 08:00hrs ás 09:00hrs</span>
-            <span>Sex das 19:00hrs ás 20:00hrs</span>
-          </p>
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">FitDance - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Segunda-feira:</strong>
+              <br />
+              <span>- 08:00hrs às 09:00hrs</span>
+              <br />
+              <span>- 19:00hrs às 20:00hrs</span>
+            </p>
+            <p>
+              <strong>Quarta-feira:</strong>
+              <br />
+              <span>- 08:00hrs às 09:00hrs</span>
+            </p>
+            <p>
+              <strong>Sexta-feira:</strong>
+              <br />
+              <span>- 08:00hrs às 09:00hrs</span>
+              <br />
+              <span>- 19:00hrs às 20:00hrs</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative col-span-2 w-full">
+        <Image
+          className="h-full absolute object-cover opacity-30 rounded-lg bg-[rgb(0,0,0,0.2)]"
+          src={ImageCrossTraining}
+          alt="Imagem de pulo"
+          objectPosition="center"
+        />
+        {/* 4 Gap, Hit 2, crosstrauining6  */}
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">Cross Training - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Terça-feira:</strong>
+              <br />
+              <span>- 19:00hrs às 20:00hrs</span>
+            </p>
+            <p>
+              <strong>Quarta-feira:</strong> <br />
+              <span> - 07:00hrs às 08:00hrs</span>
+              <br />
+              <span> - 19:00hrs às 19:00hrs</span>
+            </p>
+            <p>
+              <strong>Quinta-feira:</strong> <br />
+              <span> - 20:00hrs às 21:00hrs</span>
+            </p>
+            <p>
+              <strong>Sexta-feira:</strong> <br />
+              <span> - 07:00hrs às 08:00hrs</span>
+              <br />
+              <span> - 20:00hrs às 21:00hrs</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative w-full ">
+        <Image
+          className="h-full absolute object-cover  opacity-30 rounded-lg bg-[rgb(0,0,0,0.2)]"
+          src={ImageGap}
+          alt="Imagem de pulo"
+          objectPosition="center"
+        />
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">Gap - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Terça-feira e Quinta-feira:</strong>
+              <br />
+              <span>- 06:00hrs às 07:00hrs</span>
+            </p>
+            <p>
+              <strong>Quarta-feira:</strong>
+              <br />
+              <span>- 20:00hrs às 21:00hrs</span>
+            </p>
+            <p>
+              <strong>Sexta-feira:</strong>
+              <br />
+              <span>- 18:00hrs às 19:00hrs</span>
+            </p>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="relative w-full">
+        <Image
+          className="h-full absolute object-cover opacity-30 rounded-lg bg-[rgb(0,0,0,0.2)]"
+          src={ImageHiit}
+          alt="Imagem de pulo"
+          objectPosition="center"
+        />
+        <div className="text-white space-y-4 py-8 px-5">
+          <p className="">Hiit - Horário de Atividade</p>
+          <div className="font-extralight text-sm opacity-90 flex flex-col gap-3">
+            <p>
+              <strong>Terça-feira e Quinta-feira:</strong>
+              <br />
+              <span>- 07:00hrs às 08:00hrs</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
