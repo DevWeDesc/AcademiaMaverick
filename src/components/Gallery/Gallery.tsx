e0af9c62441ba83d8ddfe788af11ba6bb894508a
ViniHGV <ixxvinicius@gmail.com>
2024-01-15 09:38:24 -0300

    feat: adicionando estilizações que não se aplicam para mobile

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -4,11 +4,11 @@ import { SwiperGallery } from "./SwiperGallery";
 export const Gallery = () => {
   return (
     <div>
-      <div className="bg-black text-white flex flex-col px-5 pt-[100px] pb-20 gap-5 items-center lg:px-36 lg:pb-0 lg:py-28 max-w-[1700px] m-auto">
+      <div className="bg-black text-white flex flex-col px-5 pt-24 lg:pt-[100px] pb-5 gap-5 items-center lg:px-36 lg:pb-0 lg:py-28 max-w-[1700px] m-auto">
         <p className="text-logoColorLight font-semibold md:col-span-2 lg:col-span-4 lg:text-center lg:text-2xl">
           Nosso maior projeto é você!
         </p>
-        <div className="flex flex-col items-center gap-2 md:col-span-2 lg:col-span-4 mb-12">
+        <div className="flex flex-col items-center gap-2 md:col-span-2 lg:col-span-4 lg:mb-12">
           <h2 className="text-2xl text-center font-bold lg:text-5xl">
             Conheça nosso espaço!
           </h2>
