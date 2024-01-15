32e973344b2ce9d2f092becc9897b8e3cbc8f8ec
ViniHGV <ixxvinicius@gmail.com>
2024-01-15 09:37:30 -0300

    feat: adicionando quantidade de slides dinâmica de acordo com o tamanho da tela

--- src/components/Gallery/SwiperGallery.tsx
+++ src/components/Gallery/SwiperGallery.tsx
@@ -7,18 +7,27 @@ import "./styles.css";
 import { FreeMode, Pagination, Navigation } from "swiper/modules";
 import { imagesGallery } from "../../../data/data";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export const SwiperGallery = () => {
+  const [slidesPerView, setSlidesPerView] = useState(4);
+
+  useEffect(() => {
+    if (window) {
+      window.innerWidth <= 620 ? setSlidesPerView(2) : setSlidesPerView(4);
+    }
+  }, []);
+
   return (
     <Swiper
-      slidesPerView={4}
+      slidesPerView={slidesPerView}
       spaceBetween={10}
       freeMode={true}
       pagination={{
         clickable: true,
       }}
       navigation={true}
-      modules={[FreeMode, Pagination, Navigation]}
+      modules={[FreeMode, Navigation]}
       className="gallerySwiper"
     >
       {imagesGallery.map((item, index) => (
