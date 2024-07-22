9bbc809f2709f770dc77df2749f70867e08e95ce
ViniHGV <ixxvinicius@gmail.com>
2024-07-22 13:45:33 -0300

    fix: ajustando textos

--- src/components/Activies/Activities.tsx
+++ src/components/Activies/Activities.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { GridActivies } from "./GridActivies";
 import { SwiperActivies } from "../SwiperActivies/SwiperActivies";
 
 export const Activities = () => {
   return (
-    <div className="flex px-5 lg:text-center flex-col items-center gap-12 pt-16 lg:py-20 lg:px-36 bg-black max-w-[1700px] m-auto 4xl:px-0">
+    <div className="text-white flex px-5 lg:text-center flex-col items-center gap-12 pt-16 lg:py-20 lg:px-36 bg-black max-w-[1700px] m-auto 4xl:px-0">
       <div className="space-y-1">
         <div className="py-9 space-y-1">
           <h2 className="font-bold text-2xl lg:text-5xl">Nossas Atividades</h2>
@@ -16,14 +15,14 @@ export const Activities = () => {
             CrossTraining e Muito Mais!
           </p>
           <p className="opacity-60 text-[12px] hover:opacity-100 transition-all mb-2 lg:text-lg">
-            Explore uma experiência única na nossa academia, onde cada movimento
-            é um passo em direção ao seu bem-estar. Com aulas dinâmicas e
-            instrutores dedicados, oferecemos um ambiente motivador para
-            alcançar seus objetivos fitness. Seja desafiando-se em treinos
-            intensos ou relaxando em sessões de ioga revigorantes, aqui você
-            encontra a harmonia entre saúde e diversão. Descubra uma jornada
-            transformadora em nossa academia, onde cada esforço é um
-            investimento no seu melhor eu.
+            Nossas aulas coletivas são projetadas para oferecer uma experiência
+            dinâmica e envolvente para todos os nossos alunos. Com opções que
+            incluem aulas de dança, funcionais, pilates e programas especiais
+            para crianças, temos algo para cada perfil e objetivo. Cada sessão é
+            conduzida por instrutores qualificados que garantem um ambiente
+            seguro e motivador. As aulas coletivas não só proporcionam um
+            excelente treino, mas também incentivam a interação social e a
+            diversão, tornando sua rotina de exercícios mais prazerosa e eficaz.
           </p>
         </div>
       </div>
