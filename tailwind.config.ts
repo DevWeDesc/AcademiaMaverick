eaf90936e350013e7a737b21208d58d0b2f3932f
ViniHGV <ixxvinicius@gmail.com>
2024-07-22 13:43:50 -0300

    feat: criando variante de sombra

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,8 @@ const config: Config = {
       boxShadow: {
         card: "0px 0px 60px -10px rgba(0 0 0 / 0.05)",
         cardDefault: "0px 0px 200px -50px rgba(0 0 0 0 / 0.001)",
+        imageBanner:
+          "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
       },
 
       backgroundImage: {
