2e4940f96445e8bd13f9c8dce9462472b0cb3e62
ViniHGV <ixxvinicius@gmail.com>
2024-07-23 09:20:43 -0300

    fix: ajustando estilização dos cards

--- src/components/CardActivies.tsx
+++ src/components/CardActivies.tsx
@@ -6,9 +6,11 @@ type ICardActivies = {
 };
 export const CardActivies = ({ name, hour }: ICardActivies) => {
   return (
-    <div className="flex flex-col gap-5 text-[10px] p-4 lg:text-sm shadow-card shadow-logoColorLight lg:px-10 lg:py-8 rounded-lg">
+    <div className="flex flex-col gap-5 text-[10px] p-4 lg:text-sm shadow-card shadow-logoColorLight lg:px-8 lg:py-6 rounded-lg">
+      <div className="py-1 px-3 bg-orange-500 rounded-lg">
+        <p>{hour}</p>
+      </div>
       <p>{name}</p>
-      <p>{hour}</p>
     </div>
   );
 };
--- src/components/CardPartnerShips.tsx
+++ src/components/CardPartnerShips.tsx
@@ -17,7 +17,7 @@ export const CardPartnerShips = ({
     <div
       onMouseEnter={() => setTransitionFooter(true)}
       onMouseLeave={() => setTransitionFooter(false)}
-      className="px-5 py-8 shadow-cardDefault shadow-white  gap-5 rounded-lg flex flex-col items-center hover:shadow-card hover:shadow-white transition-all h-full"
+      className="px-5 pt-8 pb-24 relative shadow-cardDefault shadow-white  gap-5 rounded-lg flex flex-col items-center hover:shadow-card hover:shadow-white transition-all h-full"
     >
       <Image
         alt={`imagem da parceira ${title}`}
@@ -29,25 +29,14 @@ export const CardPartnerShips = ({
       <div>
         <p className="font-normal text-[11px] mb-3">{content}</p>
 
-        <p className="font-semibold text-sm text-center py-4">{from}</p>
-        <div
-          className={`transition-all h-[1px] m-auto bg-white ${
-            transitionFooter ? "w-10/12" : "w-3/12"
-          }`}
-        />
-        {observation.length > 0 && (
-          <p className="text-[11px] font-semibold pt-5 py-2 text-logoColorLight">
-            Observações
-          </p>
-        )}
-        {observation?.map((data, index) => (
-          <p
-            className="text-[11px] text-logoColorLight flex items-center gap-3"
-            key={index}
-          >
-            <Check size={15} /> {data}
-          </p>
-        ))}
+        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 w-full flex flex-col items-center">
+          <p className="font-semibold text-sm text-center py-4">{from}</p>
+          <div
+            className={`transition-all h-[1px] m-auto bg-white ${
+              transitionFooter ? "w-10/12" : "w-3/12"
+            }`}
+          />
+        </div>
       </div>
     </div>
   );
